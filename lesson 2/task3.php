82ae9b2882ef4eea1a1a8bce19e7e93047af87f9
Shpakouski <job.shpakouski@gmail.com>
2021-02-15 15:31:21 +0300

    lesson 2 task 3

---
+++ lesson 2/task3.php
@@ -0,0 +1,28 @@
+<?php
+//Задача 3. Написать функцию генерации календарного месяца (сделать в ассоциативном массиве, где ключом является число,
+// а значением - название дня недели). Аргументами функции являются количество дней в месяце и название первого дня в
+// месяце (например 31, "понедельник")
+
+function generateCalendarMonth($numberDays, $nameDay)
+{
+    $nameDay = mb_strtolower($nameDay);
+    $nameDays = array('понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье');
+    $calendarMonth = array();
+    for ($i = 1; $i <= $numberDays; $i++) {
+        if ($i === 1) {
+            $firstDay = array_search($nameDay, $nameDays);
+            $nameDays = array_slice($nameDays, $firstDay);
+        }
+        if ($i !== 1) {
+            $nameDays = array('понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье');
+        }
+        foreach ($nameDays as $day) {
+            $calendarMonth[$i] = $day;
+            $i++;
+            if ($i > $numberDays) break;
+        }
+    }
+    return $calendarMonth;
+}
+
+var_dump(generateCalendarMonth(30, 'Суббота'));
